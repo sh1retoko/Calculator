2ae70047ca3227bb4a1006b846076e1b49df653b
sh1retoko <sh1retoko@yandex.ru>
2023-05-05 09:20:07 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+// Калькулятор
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                double a; double b;
+                try
+                {
+                    Console.Write("Введите 1 число: ");
+                    a = double.Parse(Console.ReadLine());
+
+                    Console.Write("Введите 2 число: ");
+                    b = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Нужно вводить число!");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                Console.Write("Напишите операнд: ");
+                string c = Console.ReadLine();
+
+                switch (c)
+                {
+                    case "+":
+                        Console.WriteLine($"Ответ: {a + b}");
+                        break;
+
+                    case "-":
+                        Console.WriteLine($"Ответ: {a - b}");
+                        break;
+
+                    case "*":
+                        if (b == 0)
+                        {
+                            Console.WriteLine(0);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Ответ: {a * b}");
+                        }
+                        break;
+
+                    case "/":
+                        if (b == 0)
+                        {
+                            Console.WriteLine(0);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Ответ: {a / b}");
+                        }
+                        break;
+
+                    case "%":
+                        if (b == 0)
+                        {
+                            Console.WriteLine(0);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Ответ: {a % b}");
+                        }
+                        break;
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
